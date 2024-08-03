80bf77fa915898c7b30f6ffd9fa4b2c4161ee290
KAYOUNG KIM <lvyest.dev@gmail.com>
2024-08-04 06:53:37 +0900

    main화면버튼기능 추가

--- src/main/front/src/main/main1.js
+++ src/main/front/src/main/main1.js
@@ -1,16 +1,23 @@
 import './mainn.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import icons from './main1.svg';
 import text from './main1_text.svg';
 
 function Main1() {
+    const navigate = useNavigate(); 
+
+    const handleButtonClick = () => {
+        navigate('/'); 
+    };
+
     return (
         <div className="m1-container">
             <img src={icons} className="icons" alt="mainsvg"/>
 
             <div className="m1-textbox">
                 <img src={text} className="texts" alt ="texts"/>
-                <button className="custom-button">
+                <button className="custom-button" onClick={handleButtonClick}>
                     <span>자세히 보기</span>
                     <span className="arrow">&rarr;</span>
                 </button>
